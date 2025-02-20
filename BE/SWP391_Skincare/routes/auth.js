d5afbe17ddd59b93818af4a0b5cae271a03b2186
DatVX02 <vxdat13@gmail.com>
2025-02-20 10:17:07 +0700

    push

--- BE/SWP391_Skincare/routes/auth.js
+++ BE/SWP391_Skincare/routes/auth.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
-
 const router = express.Router();
 const { sendOTP } = require("../utils/email");
 router.post(
